5ad1fee991e24e767e837a37e04159e65f96fbea
Francesco Malagrino <francesco.malagrino@gmail.com>
2021-01-11 15:36:24 +0100

    Created Model and Repository for the Person
    Created Model and Repository for the Person

---
+++ src/main/java/com/fm/accela/AccelaTestApplication.java
@@ -0,0 +1,5 @@
+package com.fm.accela;
+
+public class AccelaTestApplication {
+
+}
---
+++ src/main/java/com/fm/accela/model/Person.java
@@ -0,0 +1,73 @@
+package com.fm.accela.model;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="person")
+public class Person {
+
+    public Person(){
+    }
+
+    public Person(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+
+
+    public Person(String firstName, String lastName) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    //Setters and getters
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+   
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
